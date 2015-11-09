b7d1fff3e59149343a209fa943de20154220d1cb
Jeffrey Macko <jeffrey@macko.fr>
2015-11-10 00:25:47 +0100

    First Commit

---
+++ TheGauntlet/TheGauntlet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheGauntlet
+//
+//  Created by Jeffrey Macko on 09/11/2015.
+//  Copyright © 2015 Jeffrey Macko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
