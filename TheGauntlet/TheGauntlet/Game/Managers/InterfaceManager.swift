e19ab4009640ae2d9fe936941e7c04e59702bb2d
Xavier De Koninck <xavier@macbook-pro-de-xavier.home>
2015-11-29 15:13:54 +0100

    test interface

---
+++ TheGauntlet/TheGauntlet/Game/Managers/InterfaceManager.swift
@@ -0,0 +1,9 @@
+//
+//  InterfaceManager.swift
+//  TheGauntlet
+//
+//  Created by Xavier De Koninck on 28/11/2015.
+//  Copyright © 2015 Jeffrey Macko. All rights reserved.
+//
+
+import Foundation
